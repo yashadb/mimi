fea3fee5b019ce1a59aaff0809641646fd59ddd6
Jasa Fridau <fridaujasa@gmail.com>
2025-02-04 19:57:55 +0100

    added new todo

--- main.go
+++ main.go
@@ -25,5 +25,6 @@ func main() {
 
 // TODO:
 // 1. figure out how to merge in seo - build seo struct, make sure config files have seo fields, reserve fields in page-config, in the template, check what fields exist and conditionally merge them in. Prefer the page config over the global one.
+// 2. Add core package (Core should hold a Mimi struct, that serves as a facade for routes, template functions and logger)
 // 4. Implement a post/page loop template function. Fun.
 // 5. Provide a very basic admin dash.
